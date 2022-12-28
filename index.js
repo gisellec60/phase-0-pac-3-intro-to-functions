c2044a511ea39c424e1a0c5f88865b5b6cdf31ba
Giselle Smith <gisellec60@gmail.com>
2022-12-28 15:14:21 -0500

    commit tree afdeb699903b36f2ae6c29e75aba77aee73bd0d0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Follow along with the examples here
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+sayHelloToSamip();
+
+function doSomething(thing) {
+  console.log(thing);
+}
+doSomething("Giselle")
+
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}`);
+}
+sayHelloTo("giselle")
+
+function say(greeting, firstName) {
+  console.log("i was called");
+  return `${greeting}, ${firstName}`;
+}
+console.log(say("Howd","Giselle"))
+  
+}
